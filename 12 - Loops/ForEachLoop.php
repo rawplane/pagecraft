4dd84085f6de008158a69167dc29052fe561e68d
R.A.W <132823989+rawplane@users.noreply.github.com>
2026-07-22 19:09:12 +0700

    Delete 12 - Loops/ForEachLoop.php

--- 12 - Loops/ForEachLoop.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// The foreach loop - Loops through a block of code for each element in an array or each property in an object.
-$colors = array("red", "green", "blue", "yellow");
-
-foreach ($colors as $x) {
-  echo "$x ";
-}
